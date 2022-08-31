c48aa47978187c6b7750a3de521735ef512c1207
Thomas Sojka <thsojka@web.de>
2022-08-31 21:33:34 +0200

    First design

--- src/tech/thomas_sojka/whodunnit/app.cljs
+++ src/tech/thomas_sojka/whodunnit/app.cljs
@@ -1,7 +1,11 @@
 (ns tech.thomas-sojka.whodunnit.app
-  (:require ["firebase/app" :as firebase]
+  (:require ["date-fns" :as date-fns]
+            ["firebase/app" :as firebase]
             ["firebase/auth" :as auth]
-            ["firebase/firestore" :as firestore]))
+            ["firebase/firestore" :as firestore]
+            [reagent.core :as r]
+            [reagent.dom :as dom]
+            [tech.thomas-sojka.whodunnit.icons :as icons]))
 
 (def initializeApp (.-initializeApp firebase))
 
@@ -26,3 +30,45 @@
       (.then (fn [snapshot] (.forEach snapshot #(-> % .data js/console.log)))))
   #_(auth/createUserWithEmailAndPassword auth "thsojka@web.de" "test123")
   (auth/signOut auth))
+
+(defn format-date [date]
+  ((.-format date-fns) date "dd.MM.yyyy HH:mm"))
+
+(defn main []
+  [:divh-full
+   [:main
+    [:header.bg-green-400
+     [:div.mx-auto.max-w-5xl.py-4.px-4
+      [:h1.text-white.text-3xl.font-bold "Whodunnit?"]]]
+    [:section.flex-col.flex.items-center.pt-8.max-w-5xl.mx-auto.px-4
+     [:div.text-4xl.pb-16.text-gray-700 (format-date (new js/Date))]
+     [:div.flex.flex-col.items-center.pb-16
+      [:div.w-36
+       [icons/icon {:class "text-red-300"} :close]]
+      [:div.text-2xl.text-center.text-red-400 "Blumen haben Durst"]]
+     [:div.flex.justify-between.w-full.pb-16
+      [:div
+       [:div.w-32.text-green-300
+        [icons/icon :check]]
+       [:div.text-2xl.text-center.text-green-400 "Ich gieße"]]
+      [:div
+       [:div.w-32.text-blue-300
+        [icons/icon :rain]]
+       [:div.text-2xl.text-center.text-blue-400 "Es soll regnen"]]]
+     [:div.w-full
+      [:h2.text-2xl.text-gray-700.pb-3 "Historie"]
+      [:ul
+       (map
+        (fn [{:keys [date userId status]}]
+          [:li.flex
+           [:span.pr-4 userId]
+           [:span.pr-4 (format-date date)]
+           [:span.pr-4 status]])
+        [{:date (new js/Date)
+          :userId "asdf"
+          :status :watering}
+         {:date (new js/Date)
+          :userId "asdf"
+          :status :watering}])]]]]])
+
+(dom/render [main] (js/document.getElementById "app"))
